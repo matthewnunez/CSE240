cd2755af60d091ec1b47da62bde369face897aad
Matthew Nunez <matt.de.nunez@gmail.com>
2016-09-11 17:21:42 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE240 
+This tracks homework assignments for ASU CSE240 class.
---
+++ hw03/asciiprint.c
@@ -0,0 +1,16 @@
+//
+// Created by Matthew Nunez on 9/10/16.
+//
+#include <stdio.h>
+int main()
+{
+    int i;
+    i=0;
+    do
+    {
+    printf("%d %c \n",i,i);
+        i++;
+    }
+    while(i<=255);
+    return 0;
+}
